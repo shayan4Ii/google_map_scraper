e8da2ef55eb0b8efe51da961d84967fbab979a3e
Shayan <109689031+shayan4Ii@users.noreply.github.com>
2025-02-12 19:50:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-"""This script serves as an example on how to use Python 
-   & Playwright to scrape/extract data from Google Maps"""
-
 from playwright.sync_api import sync_playwright
 from dataclasses import dataclass, asdict, field
 import pandas as pd
